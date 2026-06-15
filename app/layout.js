c7bfdb97a76039c4b65b76802ce77cd7d13dcd64
Icaromartinelli <icaromartinelli@gmail.com>
2026-06-15 18:25:55 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import './globals.css';
+import './globals.css'
 
 export const metadata = {
-  title: 'Ícaro Martinelli | Programas de Treino',
-  description: 'Programas de treino, consultoria online e personal trainer presencial em São Paulo.',
-};
+  title: 'Ícaro Martinelli | IM Personal',
+  description: 'Programas de treino, consultoria online e personal trainer em São Paulo.',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  );
+  )
 }
